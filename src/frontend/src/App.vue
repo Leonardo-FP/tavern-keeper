42e810726baf0c03367d784c439c137e9e8e11ce
Leonardo-FP <leopaulelli@gmail.com>
2025-08-25 11:52:53 -0300

    feat: Instalação do pacote de ícones Heroicons e ajustes visuais finais no menu superior

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -2,6 +2,7 @@
   import { ref, onMounted } from 'vue'; 
   import { RouterLink, RouterView } from 'vue-router';
   import { useAuthStore } from './stores/auth'; // Importa seu store de autenticação
+  import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/vue/24/solid'
 
   // Instancia o store de autenticação
   const authStore = useAuthStore();
@@ -17,37 +18,38 @@
 </script>
 
 <template>
-  <header class="p-4 bg-santa-fe shadow-md">
+  <header class="p-4 bg-santa-fe">
     <div class="container mx-auto flex justify-between items-center text-2xl">
       
-      <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+      <nav class="container px-6 mx-auto">
         <div class="flex items-center justify-between">
-            <RouterLink to="/" class="text-4xl font-bold text-black">Tavern Keeper</RouterLink>
+            <RouterLink to="/" class="text-4xl font-bold">Tavern Keeper</RouterLink>
             <!-- Mobile menu button -->
-            <div @click="toggleNav" class="flex md:hidden">
-              <button type="button" class="focus:outline-none">
-                  <img class="rounded-full w-10 h-10 object-cover cursor-pointer" :src="authStore?.user?.icon?.path ?? '/assets/icons/avatars/beer.png'" alt="">
+            <div @click="toggleNav" class="flex">
+              <button type="button" class="focus:outline-none flex cursor-pointer">
+                  <img class="rounded-full bg-black shadow-xl/30 border-1 p-1 w-16 h-16 object-cover" :src="authStore?.user?.icon?.path ?? '/assets/icons/avatars/beer.png'" alt="Ícone escolhido pelo usuário para seu perfil">
+                  <ChevronDownIcon v-if="!showMenu" class="ml-2 w-5" />
+                  <ChevronUpIcon v-else class="ml-2 w-5" />
               </button>
           </div>
         </div>
-
-        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+      </nav>
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col ml-auto mt-24 text-right fixed top-0 right-0 left-0 bg-black text-santa-fe w-48">
           <template v-if="!authStore.isAuthenticated">
-            <li><RouterLink to="/about" class="px-4 py-2 text-black hover:text-di-serria">About</RouterLink></li>
-            <li><RouterLink to="/login" class="px-4 py-2 text-black hover:text-di-serria">Login</RouterLink></li>
-            <li><RouterLink to="/register" class="px-4 py-2 text-black hover:text-di-serria">Register</RouterLink></li>
+            <li><RouterLink to="/about" class="px-4 py-2 hover:text-di-serria">About</RouterLink></li>
+            <li><RouterLink to="/login" class="px-4 py-2 hover:text-di-serria">Login</RouterLink></li>
+            <li><RouterLink to="/register" class="px-4 py-2 hover:text-di-serria">Register</RouterLink></li>
           </template>
 
           <template v-else>
-            <li><RouterLink to="/" class="px-4 py-2 text-black hover:text-di-serria">Minhas Mesas</RouterLink></li>
-            <li><RouterLink to="/" class="px-4 py-2 text-black hover:text-di-serria">Meu Perfil</RouterLink></li>
-            <li><RouterLink to="/" class="px-4 py-2 text-black hover:text-di-serria">Minhas Campanhas</RouterLink></li>
-            <li><RouterLink to="/" class="px-4 py-2 text-black hover:text-di-serria">Home</RouterLink></li>
-            <li><button @click="authStore.logout()" class="px-4 py-2 text-di-serria cursor-pointer">Logout</button></li>
+            <li><RouterLink to="/" class="px-4 py-2 hover:text-di-serria">Minhas Mesas</RouterLink></li>
+            <li><RouterLink to="/" class="px-4 py-2 hover:text-di-serria">Meu Perfil</RouterLink></li>
+            <li><RouterLink to="/" class="px-4 py-2 hover:text-di-serria">Minhas Campanhas</RouterLink></li>
+            <li><RouterLink to="/" class="px-4 py-2 hover:text-di-serria">Home</RouterLink></li>
+            <li><button @click="authStore.logout()" class="px-4 py-2 hover:text-di-serria cursor-pointer">Logout</button></li>
           </template>
         </ul>
-      </nav>
     </div>
   </header>
 
